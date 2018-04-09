13236f3da99409b205d621f16b0baaef7a36f2e9
Rishabh Narang <radon@Rishabhs-MacBook-Pro.local>
2018-04-09 17:47:12 +0530

    added 12 questions.. data preprocessing, data transformation done.. left with data information (partially) and apriori

--- Readme.md
+++ Readme.md
@@ -10,3 +10,10 @@ List of questions :
 3. Who are the top 20 customers based on the shopping amount that they spent ?
 4. What are the frequently sold items by quantitiy ?
 5. What are the frequently sold items by total amount ?
+6. Analyse number of sales for every month.
+7. What are the monthly earnings of the company ?
+8. Region (Country) wise sales.
+9. What are the number of active customers in each country ?
+10. Get United Kingdom top ranked customers based on the total amount.
+11. What are United Kingdom's frequently sold items by quantitiy ?
+12. What are United Kingdom's frequently sold items by total amount ?
--- association.py
+++ association.py
@@ -65,7 +65,78 @@ plt.show()
 #Ques Frequently sold items by total amount
 gp_stockcode_frq_amount = gp_stockcode['TotalAmount'].agg(np.sum).sort_values(ascending=False)
 print("\n\n-----------Frequently sold items by total amount: -----------")
-gp_stockcode_frq_amount.head(20)
+print(gp_stockcode_frq_amount.head(20))
 
 gp_stockcode_frq_amount.head(20).plot.bar()
 plt.show()
+
+
+# Ques Number of sales each month
+gp_month = dataset.sort_values('InvoiceDate').groupby(['InvoiceYear', 'InvoiceMonth'])
+gp_month_invoices = gp_month['InvoiceNo'].unique().agg(np.size)
+print("\n\n-----------Number of sales each month: -----------")
+print(gp_month_invoices)
+
+gp_month_invoices.plot.bar()
+plt.show()
+
+
+# Ques Monthly earnings
+gp_month_frq_amount= gp_month['TotalAmount'].agg(np.sum)
+print("\n\n-----------Monthly earnings: -----------")
+print(gp_month_frq_amount)
+
+gp_month_frq_amount.plot.bar()
+plt.show()
+
+
+#Que Region(Country) wise sales
+gp_country = dataset.groupby('Country')
+print("\n\n-----------Regional sales: -----------")
+print(gp_country['TotalAmount'].agg(np.sum).sort_values(ascending=False))
+
+
+#Ques Number of active customers in each country
+print("\n\n-----------Number of active customers in each country: -----------")
+print(gp_country['CustomerID'].unique().agg(np.size).sort_values(ascending=False))
+
+
+#Ques Get United Kingdom top 20 customers based on the total amount
+uk_customers_amounts = dataset[dataset['Country']=='United Kingdom'].groupby('CustomerID')['TotalAmount'].agg(np.sum).sort_values(ascending=False)
+print("\n\n-----------United Kingdom's top 20 customers based on the total amount: -----------")
+print(uk_customers_amounts.head(20))
+
+uk_customers_amounts.head(20).plot.bar()
+plt.show()
+
+
+#Ques United Kingdom frequently sold items by quantitiy
+uk_gp_stockcode = dataset[dataset['Country']=='United Kingdom'].groupby('Description')
+uk_gp_stockcode_frq_quantitiy = uk_gp_stockcode['Quantity'].agg(np.sum).sort_values(ascending=False)
+print("\n\n-----------United Kingdom's frequently sold items by quantitiy: -----------")
+print(uk_gp_stockcode_frq_quantitiy.head(20))
+
+uk_gp_stockcode_frq_quantitiy.head(20).plot.bar()
+plt.show()
+
+
+
+#Ques United Kingdom frequently sold items by total amount
+uk_gp_stockcode_frq_amount = uk_gp_stockcode['TotalAmount'].agg(np.sum).sort_values(ascending=False)
+print("\n\n-----------United Kingdom's frequently sold items by total amount: -----------")
+print(uk_gp_stockcode_frq_amount.head(20))
+
+uk_gp_stockcode_frq_amount.head(20).plot.bar()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
